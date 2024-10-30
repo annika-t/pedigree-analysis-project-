382a2a9d195cfea53adf89dcc8bb5692b7d818f4
Tracy Chan <149117687+TracyC123@users.noreply.github.com>
2024-10-31 06:02:32 +0800

    Update run.py

--- run.py
+++ run.py
@@ -14,13 +14,11 @@ PEDIGREE={}
 IFAMILIES=[]
 #dictionary to store individual family member (id, chars)
 PEOPLE={} 
-FAMNUM=20
 
-def create_people(FAMNUM):
-    while i <=FAMNUM:
-        id=i
-        PEOPLE.update({id: None})
-        
+def create_person(id, gender, affected, br):
+    char=[gender, affected, br]
+    PEOPLE.update({id: char})
+
 def create_ifam(person1, person2, person3=None): #parameters are id of people 
     # Helper function to add a person to a list without duplicates
     def add_to_list(person, list_name):
@@ -34,41 +32,36 @@ def create_ifam(person1, person2, person3=None): #parameters are id of people
         add_to_list(person2, "siblings")
     else:
         # Three arguments case: first goes to siblings, second and third go to parents
-        add_to_list(person1, "siblings")
-        add_to_list(person2, "parents")
-        add_to_list(person3, "parents")
+        if (person2.gender != person3.gender):
+            add_to_list(person1, "siblings")
+            add_to_list(person2, "parents")
+            add_to_list(person3, "parents")
+        else:
+            raise ValueError("Parents must be of opposite genders.")
     IFAMILIES.append(ifamily)
     
 def create_pedigree(IFAMILIES):
 
 
+
+
+
+    
 # To create propositions, create classes for them first, annotated with "@proposition" and the Encoding
 #examplar:
 @proposition(E)
 class Char(object):
-    def assign_person(id, char):
-        characteristic=[0,0,0]
-        if char="Female":
-            char[0]=1
-        elif char="Blood Relative":
-            char[1]=1
-        elif char="Affected":
-            char[2]=1
-        PEOPLE[id]=characteristic
-    def get_person:
-        if if in PEOPLE:
-            if id <=FAMNUM:
-                return PEOPLE[id]
+    def __init__(self, id, char):
+        assert id in PEOPLE
+        assert char in PEOPLE
+        self.id=id
+        self.char=char
     def _prop_name(self):
-        return f"Char.{self.id}={self.char}"
+        return f"Char.{self.id}+={self.char}"
 
 @proposition(E)
 class Rel(object):
-    def __init__(self,family1,family2=None):
-        if family2=None:
-            assert family1 in FAMILY
-            self.family1 = family1
-        else:
+    def __init__(self,family1,family2):
         assert family1 in FAMILY
         assert family2 in FAMILY
         self.family1 = family1
@@ -76,6 +69,61 @@ class Rel(object):
 
     def _prop_name(self):
         return f"fam({self.char}"
+
+
+#our project:
+#Characteristics of each family member
+@proposition(E)
+class Female(object):
+    def _init_(self, person_id):
+        assert person in PEOPLE
+        self.person_id =person_id
+
+    def _prop_name(self):
+        return f"F({self.person_id})=True"
+
+@proposition(E)
+class Affection(object):
+    def _init_(self, person_id):
+        assert person_id in PEOPLE
+        self.person_id = person_id
+
+    def _prop_name(self):
+        return f"F({self.person_id})=True"
+
+@proposition(E)
+class BloodRelative:
+    def _init_(self, person_id):
+        assert person_id in PEOPLE
+        self.person_id = person_id
+
+    def _prop_name(self):
+        return f"F({self.person_id})=True"
+
+
+#Relationship between family members
+@proposition(E)
+class Child:
+    def _init_(self, child_id, parent1_id, parent2_id):
+        assert child_id, parent1_id, parent_id in PEOPLE
+        self.child_id = child_id
+        self.parent1_id = parent1_id
+        self.parent2_id = parent2_id
+
+    def _prop_name(self):
+        return f"C({self.child_id}, {self.parent1_id}, {self.parent2_id})=True"
+        
+
+
+@proposition(E)
+class Sibling:
+    def _init_(self, person1_id, person2_id):
+        assert person1_id, person2_id in PEOPLE
+        self.person1_id = person1_id
+        self.person2_id = person2_id
+
+    def _prop_name(self):
+        return f"C({self.perso1n_id}, {self.person2_id})=True"
 
 #Inheritance Pattern
 @proposition(E)
@@ -105,11 +153,11 @@ class AtLeastOneAffected():
     
 
 # Initialize propositions
-f = Char("Female")   
-a = Char("Affected")
-r = Char("Blood Relative")
-c = Rel(person_id, parent1_id, parent2_id)
-s = Rel(person1_id, person2_id)
+f = Female(person_id)
+a = Affected(person_id)
+r = BloodRelative(person_id)
+c = Child(person_id, parent1_id, parent2_id)
+s = Sibling(person1_id, person2_id)
 m = MoreMalesAffected(generation)
 r_mode = RecessiveDisease()
 x_mode = XLinkedDisease()
